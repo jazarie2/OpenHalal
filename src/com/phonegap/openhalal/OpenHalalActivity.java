9c68d6c649e19c369f16b255d0574a0331beee7b
unknown <jazarie2@.(none)>
2012-03-16 22:10:14 +0800

    Adding Basic PhoneGap Structure

---
+++ src/com/phonegap/openhalal/OpenHalalActivity.java
@@ -0,0 +1,14 @@
+package com.phonegap.openhalal;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class OpenHalalActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
